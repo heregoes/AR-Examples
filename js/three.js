e1eba94ec374baa866ac3d4199f783bc55202a25
Gabriel Nyante <gabnyante@gmail.com>
2021-03-23 11:08:25 -0400

    updated hello-cube.html

--- js/three.js
+++ js/three.js
@@ -40049,22 +40049,24 @@
 		},
 
 		setFromCamera: function ( coords, camera ) {
-
-			if ( ( camera && camera.isPerspectiveCamera ) ) {
+			// console.log('camera', camera)
+			// console.log('ray', this.ray)
+			// if ( ( camera && camera.isPerspectiveCamera ) ) {
 
 				this.ray.origin.setFromMatrixPosition( camera.matrixWorld );
 				this.ray.direction.set( coords.x, coords.y, 0.5 ).unproject( camera ).sub( this.ray.origin ).normalize();
 
-			} else if ( ( camera && camera.isOrthographicCamera ) ) {
+			// } 
+			// else if ( ( camera && camera.isOrthographicCamera ) ) {
 
-				this.ray.origin.set( coords.x, coords.y, ( camera.near + camera.far ) / ( camera.near - camera.far ) ).unproject( camera ); // set origin in plane of camera
-				this.ray.direction.set( 0, 0, - 1 ).transformDirection( camera.matrixWorld );
+			// 	this.ray.origin.set( coords.x, coords.y, ( camera.near + camera.far ) / ( camera.near - camera.far ) ).unproject( camera ); // set origin in plane of camera
+			// 	this.ray.direction.set( 0, 0, - 1 ).transformDirection( camera.matrixWorld );
 
-			} else {
+			// } else {
 
-				console.error( 'THREE.Raycaster: Unsupported camera type.' );
+			// 	console.error( 'THREE.Raycaster: Unsupported camera type.' );
 
-			}
+			// }
 
 		},
 
